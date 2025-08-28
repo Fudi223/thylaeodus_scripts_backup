56dfe996fe5a8f36ee9249f9fed972aafc391939
Fudi223 <fdudi@hotmail.com>
2025-08-28 11:50:51 +0200

    update

--- scripts/cdhit_trim.py
+++ scripts/cdhit_trim.py
@@ -3,13 +3,19 @@
 # cdhit cluster trimming script
 
 import os
+import re
 from tqdm import tqdm # type: ignore  # noqa: F401
 
 path_dir = os.path.dirname(os.path.realpath(__file__))
 file_name = os.path.join(path_dir, "test_cluster.clstr")
 
 with open(file_name, "r") as file:
-    blocki = ()
-    for blocks in file.readlines():
-        if blocks[0] == ">":
-            print(blocki)
+    line_list = []
+    for lines in file.readlines():
+        line = lines.strip("\n")
+        if line.startswith(">Cluster"):
+            continue
+        if "_g" in line:
+            line_list.append(line)
+    print(line_list)
+ 
